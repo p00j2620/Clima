73b257e13fe421fcd0e381d0ab6f444b5718c1a9
Arthur Pujols <arthurpujols@Arthurs-MacBook-Pro.local>
2018-05-17 11:18:04 -0400

    Added cocoapods, enabled location manager, properly accessing location data, and created API call link

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -7,16 +7,19 @@
 //
 
 import UIKit
+import CoreLocation
 
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
-    let APP_ID = "e72ca729af228beabd5d20e3b7749713"
+   
+    let APP_ID = "1da293ddb8d4d631253c1bab17d8a3ad"
     
 
     //TODO: Declare instance variables here
+    let locationManager = CLLocationManager()
     
 
     
@@ -31,6 +34,10 @@ class WeatherViewController: UIViewController {
         
         
         //TODO:Set up the location manager here.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     
         
         
@@ -75,12 +82,32 @@ class WeatherViewController: UIViewController {
     
     
     //Write the didUpdateLocations method here:
-    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations[locations.count - 1]
+        
+        if location.horizontalAccuracy > 0 {
+            locationManager.stopUpdatingLocation()
+            
+            let latitude = String(location.coordinate.latitude)
+            let longitude = String(location.coordinate.longitude)
+            
+            let params: [String : String] = ["lat": latitude, "lon": longitude, "appid": APP_ID]
+            
+            let WEATHER_COORDS = "?lat=\(params["lat"]!)&lon=\(params["lon"]!)&appid=\(params["appid"]!)"
+            
+            // ?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22
+           print(WEATHER_URL + WEATHER_COORDS)
+        }
+    }
     
     
     //Write the didFailWithError method here:
     
-    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+        cityLabel.text = "Location Unavailable"
+    }
     
 
     
